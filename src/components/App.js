d4da06f6899ca9739b8ca7a80a2a336182c989ad
Alireza <alirezamolaii@gmail.com>
2022-03-03 16:45:17 +0330

    init propject

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ContactForm from "./ContactForm";
+import ContactList from "./ContactList";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contactList: ["asghar", "akbar", "mehrnaz", "abbas"],
+    };
+  }
+
+  handleAddContact = (contact) => {
+    this.setState({ contactList: [...this.state.contactList, contact] });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <ContactForm onAddContact={this.handleAddContact} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ContactList contacts={this.state.contactList} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
